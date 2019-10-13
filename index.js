3ef37f5c9181d5ff9ccd4d8c16bb1ee04d83a6e5
jenkko <angel_01@hotmail.es>
2019-10-13 16:32:45 -0500

    killer info

--- index.js
+++ index.js
@@ -12,25 +12,6 @@ let guild;
 
 client.login("NjMyNzMxOTkxNjQ5MDI2MDY4.XaKiCA.s02GkprG8Eo9Se0C0FQAXXmBLhM");
 
-events()
-    .then((results) => {
-        lastKiller = results[0].Killer;
-        lastVictim = results[0].Victim;
-        itemData(lastKiller.Equipment.MainHand.Type).then( item => {
-            
-            lastkillerWeapon = item.localizedNames["ES-ES"];
-            console.log(lastkillerWeapon);
-            console.log(lastKiller.Name);
-            console.log(lastVictim.Name);
-            return(0);
-        }).catch( err => {
-            console.log(err);
-        });
-        
-    }).catch(err => {
-        console.log(err);
-    });
-
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
