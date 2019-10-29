5640467005cdf6e6ff64de7815f6c212c9a8268c
jenkko <angel_01@hotmail.es>
2019-10-29 15:40:05 -0500

    async bug fixed

--- index.js
+++ index.js
@@ -10,23 +10,30 @@ let lastVictim;
 
 let guild;
 
+
+
 client.login("NjMyNzMxOTkxNjQ5MDI2MDY4.XaKiCA.s02GkprG8Eo9Se0C0FQAXXmBLhM");
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+getLastKill();
+
 client.on('message',  message => {
     if (message.content === "last kill"){
         getLastKill().then(() => {
-            const embed = new RichEmbed()
+            getItemName().then(() => {
+                const embed = new RichEmbed()
                 .setTitle('Ultima kill')
                 .setColor(0x00EAFF)
                 .addField("Asesino:", lastKiller.Name)
                 .addField("Victima", lastVictim.Name)
                 .addField("Arma del asesino", lastkillerWeapon)
                 .setThumbnail(`https://gameinfo.albiononline.com/api/gameinfo/items/${lastKiller.Equipment.MainHand.Type}`)
-            message.channel.send(embed);
+                message.channel.send(embed);
+            });
+            
         }).catch( () => {
             console.log("error");
         });
@@ -39,18 +46,20 @@ async function getLastKill(){
     .then((results) => {
         lastKiller = results[0].Killer;
         lastVictim = results[0].Victim;
-        itemData(lastKiller.Equipment.MainHand.Type).then( item => {
-            lastkillerWeapon = item.localizedNames["ES-ES"];
-            console.log(lastkillerWeapon);
-            console.log(lastKiller.Name);
-            console.log(lastVictim.Name);
-            return(0);
-        }).catch( err => {
-            console.log(err);
-        });
-        
-    }).catch(err => {
+    })
+    .catch(err => {
         console.log(err);
+        return 0;
     });
 
+}
+
+async function getItemName(){
+    await itemData(lastKiller.Equipment.MainHand.Type)
+    .then( item => {
+        lastkillerWeapon = item.localizedNames["ES-ES"];
+    }).catch( err => {
+        console.log(err);
+        return 0;
+    });
 }
